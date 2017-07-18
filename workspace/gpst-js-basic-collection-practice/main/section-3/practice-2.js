e48d5a0bc03365a575478debed98be52d6dc7867
zero654655902 <929936270@qq.com>
2017-07-18 16:06:00 +0800

    section-3-practice-2

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-2.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  // return '实现练习要求，并改写该行代码。';
+    var arr=objectB.value;
+    for(var i=0; i<arr.length; ++i){
+        for(var j=0; j<collectionA.length; ++j)
+        {
+            if(arr[i]==collectionA[j].key)
+            {
+
+              var n=(collectionA[j].count%3);
+              n=(collectionA[j].count-n)/3;
+                collectionA[j].count-=n;
+
+            }
+        }
+
+    }
+    return collectionA;
 }
