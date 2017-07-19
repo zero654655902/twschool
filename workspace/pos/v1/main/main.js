220754fa233c542d2032b6b7d92e60fe3f70199d
zero654655902 <929936270@qq.com>
2017-07-19 16:55:33 +0800

    1、统计相同字符串的个数，2、商品个数满3减一

--- workspace/pos/v1/main/main.js
+++ workspace/pos/v1/main/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+module.exports = function collectSameElements(collection) {
+    let result = [];
+    for (let i = 0; i < collection.length; ++i) {
+        let index = isInArr(collection[i], result);
+        if (index == -1) {
+            result.push({key: collection[i], count: 1});
+        }
+        else {
+            result[index].count++;
+        }
+    }
+    return result;
+
+}
+
+function isInArr(element,strArr) {
+    let index=-1;
+    for(let i=0;i<strArr.length;++i) {
+        if(element===strArr[i].key) {
+            //元素在数组中已经找到，并返回下标
+            index=i;
+        }
+    }
+    return index;
+}
+
+function isInItem(element,allItemCollection){
+    for(let i=0;i<allItemCollection[i].length;++i){
+        if(element===allItemCollection[i].barcode)
+        {
+            return true;
+        }
+
+    }
+    return false;
+}
+function processingDate(collectionA,collectionB) {
+    for (let i=0;i<collectionA.length;++i) {
+        let tmpNum=collectionA[i].count;
+        let prom=(tmp-tmp%3)/3;
+        collectionB.push(collectionB[i].key,prom);
+    }
+}
+
+function caclulatePrice(collectionB,allItemsCollection) {
+
+    for(let i=0; i<collectionB.length; ++i) {
+
+
+    }
+}
+
+
