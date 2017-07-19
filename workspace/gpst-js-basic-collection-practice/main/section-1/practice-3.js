61ac9bb087f35dca20d0409e0e3486f9e0608843
zero654655902 <929936270@qq.com>
2017-07-19 09:57:25 +0800

    section1-practice3-refactor

--- workspace/gpst-js-basic-collection-practice/main/section-1/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-1/practice-3.js
@@ -16,7 +16,7 @@ function isInArr(element,strArr) {
     for(let i=0;i<strArr.length;++i) {
         if(element===strArr[i]) {
             flag=true;
-            break;        //元素在数组中已经找到，跳出不在查找该元素
+            break;        //元素在数组中已经找到，跳出不再查找该元素
         }
     }
     return flag;
