28dcfb99550f952cfd5d685469ffa6df8da3e9ec
zero654655902 <929936270@qq.com>
2017-07-21 00:12:44 +0800

    printItemMassege

--- workspace/pos/v2/main/main.js
+++ workspace/pos/v2/main/main.js
@@ -17,7 +17,7 @@
  ];
 
 */
-function printTag(tags) {
+function printReceipt(tags) {
     let tagCollection=tagsSplit(tags);
     let allItems=loadAllItems();
     let tagsItem=buildItem(tagCollection,allItems);
@@ -56,7 +56,7 @@ function buildItem(collection,allItems){
 
     for(let i=0;i<collection.length;++i)
     {
-        let index=indexOfItem(collection[i].barcode,allItems);
+        let index=getIndexOfCollection(collection[i].barcode,allItems);
         // allItems[index].count=collection[i].count;
 
         tagsItem.push({barcode:allItems[index].barcode,name:allItems[index].name,
@@ -94,6 +94,15 @@ function calculateItemCount(collection) {
     return itemMassege;
 }
 
+function isInArr(element,strArr){
+    for(let i=0;i<strArr.length;++i){
+        if(element===strArr[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
 function isDiscount(element,promotion) {
     if(isInArr(element.barcode,promotion)&&element.count>=3)
     {
@@ -131,6 +140,35 @@ function priceMassege(allItemsMassege)
     }
     lastPrice.push({总计:totalPrice.toFixed(2),节省:diffPrice.toFixed(2)});
     return lastPrice;
+}
+
+function getCurrentDate() {
+    let currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds());
+        return `${year}年${month}月${date}日${hour}${minute}${second}`;
+
+}
+
+function printMassege(allItemsMassege,lastPrice) {
+    let storeName=`***<没钱赚商店>收据***\n`;
+    let printTime=`打印时间：${getCurrentDate()}\n`
+   let itemsMassege="";
+    for(let i=0;i<allItemsMassege.length;++i){
+        itemsMassege+='名称：'+(allItemsMassege[i].name)
+            +', 数量：'+(allItemsMassege[i].count)+(allItemsMassege[i].unit)
+            +', 单价：'+(allItemsMassege[i].price.toFixed(2))+'(元)'
+            +', 小计：'+(allItemsMassege[i].totalPrice.toFixed(2))+'元';
+    }
+    let totalPriceText='总计：'+lastPrice[0].totalPrice+'元\n';
+    let diffPriceText='节省：'+lastPrice[0].diffPrice+'元\n';
+    let result=storeName+printTime+'----------------------\n'+itemsMassege+'----------------------'+totalPriceText+diffPriceText+'**********************';
+    return result;
+
 }
 
 
