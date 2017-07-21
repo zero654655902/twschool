5fb4832075e32afb7a2c83d9c64b51e2c920d38c
zero654655902 <929936270@qq.com>
2017-07-21 16:59:39 +0800

    practice_1 pass test 预计10min, 实际15min

--- workspace/oop-dojo-master/spec/practice_1/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_1/main_spec.js
@@ -1,7 +1,11 @@
 "use strict";
+import Person from '../../main/practice_1.js'
+describe("OO入门", function() {
+    it("写一个类-1", function () {
 
-describe("OO入门", function(){
-    it("写一个类-1", function(){
-	console.log(‘first change’)
+        let person = new Person('Tom', 21)
+
+        expect((person.introduce()).toEqual('My name is Tom. I am 21 years old.'))
+
     });
 });
