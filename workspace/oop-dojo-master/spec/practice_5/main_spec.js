f2e7b5e63c07a4a7f547f83f44408b2ff76f693a
zero654655902 <929936270@qq.com>
2017-07-21 20:37:44 +0800

    practice_5 pass test 预计20min，实际23min

--- workspace/oop-dojo-master/spec/practice_5/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_5/main_spec.js
@@ -1,7 +1,14 @@
 "use strict";
-
+import {Person,Student,Worker} from '../../main/practice_5'
 describe("OO入门", function(){
     it("覆盖同时复用父类的方法", function(){
-
+        let person=new Person('Tom',21)
+        let student=new Student('Tom',21,2)
+        let worker=new Worker('Jorn',32)
+        expect(person.introduce()).toEqual('My name is Tom. I am 21 years old.')
+        expect(person.introduce()+' '+student.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.')
+        expect(person.introduce()+' '+worker.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Worker. I have a job.')
     });
 });
+
+
