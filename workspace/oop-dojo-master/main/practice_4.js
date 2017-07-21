f7d0939e1e97f2671738047c9059b7c465a25fc4
zero654655902 <929936270@qq.com>
2017-07-21 20:05:19 +0800

    practice_4 pass test0m，实际12min

---
+++ workspace/oop-dojo-master/main/practice_4.js
@@ -0,0 +1,37 @@
+class Person{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    introduce(){
+        return 'My name is '+this.name+'. I am '+this.age+' years old'
+    }
+}
+
+class Student extends Person{
+    constructor(name,age,classNum)
+    {
+        super(name,age)
+        this.classNum=classNum
+    }
+    introduce(){
+        return 'I am a Student. I am at Class '+this.classNum+'.'
+    }
+
+}
+
+class Worker extends Person{
+    constructor(name,age)
+    {
+        super(name,age)
+    }
+    introduce(){
+        return 'I am a Worker. I have a job.'
+    }
+}
+
+module.exports={
+    Person,
+    Student,
+    Worker
+}
--- workspace/oop-dojo-master/spec/practice_4/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_4/main_spec.js
@@ -1,7 +1,14 @@
 "use strict";
 
+import {Person,Student,Worker} from '../../main/practice_4'
 describe("OO入门", function(){
     it("覆盖父类的方法-2", function(){
+        let person=new Person('Tom',21)
+        let student=new Student('Tom',21,2)
+        let worker=new Worker('Jorn',32)
+        expect(person.introduce()).toEqual('My name is Tom. I am 21 years old')
+        expect(student.introduce()).toEqual('I am a Student. I am at Class 2.')
+        expect(worker.introduce()).toEqual('I am a Worker. I have a job.')
 
     });
 });
