4ded748f748ebf46f6117f8d0c0f675ff619aebd
zero654655902 <929936270@qq.com>
2017-07-23 11:12:09 +0800

    practice_9 test case one pass

--- workspace/oop-dojo-master/main/practice_9.js
+++ workspace/oop-dojo-master/main/practice_9.js
@@ -1,16 +1,21 @@
 class Person{
-    constructor(name,age)
+    constructor(id,name,age)
     {
+        this.id=id
         this.name=name
         this.age=age
+
     }
     introduce()
     {
         return 'My name is '+this.name+', I am '+this.age+' years old.'
     }
+    isSamePerson(person){
+        return (this.id===person.id)
+    }
 }
 
 
 module.exports={
-    Person,
+    Person
 }
--- workspace/oop-dojo-master/spec/practice_9/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_9/main_spec.js
@@ -2,10 +2,10 @@
 import {Person,Student,Teacher,Class} from '../../main/practice_9'
 describe("OO入门", function(){
     it("对象之间的交互-3", function(){
-        let person=new Person('Tom',21)
+        let person=new Person(11,'Tom',21)
         expect(person.introduce()).toEqual('My name is Tom, I am 21 years old.')
-        //let teacher=new Teahcer('Jorn',32)
-
+        let person2=new Person(12,'Tom',21)
+        expect(person.isSamePerson(person2)).toEqual(false)
 
     });
 });
