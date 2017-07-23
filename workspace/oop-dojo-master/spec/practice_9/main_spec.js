a38d24eae02f3d2755f15d3e5656be43393df549
zero654655902 <929936270@qq.com>
2017-07-23 12:18:37 +0800

    practice_9, test pass [expect:50min, actual:65min]

--- workspace/oop-dojo-master/spec/practice_9/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_9/main_spec.js
@@ -1,5 +1,5 @@
 "use strict";
-import {Person,Student,Class} from '../../main/practice_9'
+import {Person,Student,Class,Teacher} from '../../main/practice_9'
 describe("OO入门", function(){
     it("对象之间的交互-3", function(){
         let person=new Person(11,'Tom',21)
@@ -14,6 +14,11 @@ describe("OO入门", function(){
         _class2.assignLeader(student2)
         expect(student.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am at class 2')
         expect(student2.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am the leader of class 2')
+
+        let teacher=new Teacher(23,'Tom',21)
+        let teacher2=new Teacher(23,'Tom',21,_class)
+        expect(teacher.introduce()).toEqual('My name is Tom, I am 21 years old. I am a teacher, I teach No Class.')
+        expect(teacher2.introduce()).toEqual('My name is Tom, I am 21 years old. I am a teacher, I teach Class 2.')
 
     });
 });
