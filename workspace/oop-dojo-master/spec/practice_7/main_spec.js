b1286d5d225bcc915dc38bfcb90141bbd87a9a29
zero654655902 <929936270@qq.com>
2017-07-23 16:52:04 +0800

    refactor practice_7 test pass

--- workspace/oop-dojo-master/spec/practice_7/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_7/main_spec.js
@@ -3,15 +3,15 @@
 import {Person,Student,Worker,Teacher} from '../../main/practice_7'
 describe("OO入门", function(){
     it("对象之间的交互-1", function(){
-        let person=new Person('Tom',21)
-        let student=new Student('Tom',21,2)
-        let worker=new Worker('Jorn',32)
-        let teacher=new Teacher('Lina',28)
-        let teacher2=new Teacher('Lina',28,2)
+        const person=new Person('Tom',21)
+        const student=new Student('Tom',21,2)
+        const worker=new Worker('Jorn',32)
+        const teacher=new Teacher('Lina',28)
+        const teacher2=new Teacher('Lina',28,2)
         expect(person.introduce()).toEqual('My name is Tom, I am 21 years old.')
-        expect(person.introduce()+' '+student.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am at Class 2.')
-        expect(person.introduce()+' '+worker.introduce()).toEqual('My name is Tom, I am 21 years old. I am a Worker, I have a job.')
-        expect(person.introduce()+' '+teacher.introduce()).toEqual('My name is Tom, I am 21 years old. I am a teacher, I teach No Class.')
-        expect(person.introduce()+' '+teacher2.introduce()).toEqual('My name is Tom, I am 21 years old. I am a teacher, I teach Class 2.')
+        expect(student.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am at Class 2.')
+        expect(worker.introduce()).toEqual('My name is Jorn, I am 32 years old. I am a Worker, I have a job.')
+        expect(teacher.introduce()).toEqual('My name is Lina, I am 28 years old. I am a teacher, I teach No Class.')
+        expect(teacher2.introduce()).toEqual('My name is Lina, I am 28 years old. I am a teacher, I teach Class 2.')
     });
 });
