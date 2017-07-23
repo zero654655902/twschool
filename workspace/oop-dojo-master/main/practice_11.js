5482c91bc619232faa35e2b2e6ee0d49deb0b58d
zero654655902 <929936270@qq.com>
2017-07-23 16:28:04 +0800

    practice_11 not pass

---
+++ workspace/oop-dojo-master/main/practice_11.js
@@ -0,0 +1,126 @@
+
+class Person{
+    constructor(id,name,age)
+    {
+        this.id=id
+        this.name=name
+        this.age=age
+
+    }
+    introduce()
+    {
+        return `My name is ${this.name}, I am ${this.age} years old.`
+    }
+    isSamePerson(person){
+        return (this.id===person.id)
+    }
+}
+
+class Class{
+    constructor(number)
+    {
+        this.number=number
+    }
+    getClassNum()
+    {
+        return this.number
+    }
+    assignLeader(student)
+    {
+        if(this.student==student)
+            this.leader=student
+
+    }
+    isLeader(){
+        return this.leader
+    }
+    appendMember(student){
+        this.student=student
+    }
+    isClassMember()
+    {
+        return this.student
+    }
+    isIn(student){
+        return student._class.number===number
+
+    }
+
+}
+
+class Student extends Person{
+    constructor(id,name,age,_class)
+    {
+        super(id,name,age)
+        this._class=_class
+    }
+
+    introduce()
+    {
+
+        if(!this._class.isClassMember())
+        {
+            return `It is not of us.`
+
+        }
+        else if(this._class.isLeader())
+        {
+            return `${super.introduce()} I am a student, I am the leader of Class ${this._class.number}.`
+
+        }
+        else
+            return `${super.introduce()} I am a student, I am at Class ${this._class.number}.`
+
+    }
+}
+
+class Teacher extends Person{
+    constructor(id,name,age,classes)
+    {
+        super(id,name,age)
+        this.classes=classes
+    }
+    introduce(){
+        if(this.classes==null)
+            return `${super.introduce()} I am a teacher, I teach No Class.`
+
+        else
+        {
+            let str=`${super.introduce()} I am a teacher, I teach Class ${this.classes.number}`
+            //for(let _class in this.classes)
+           // {
+            //    str+=` ${_class.getClassNum()}`
+           // }
+           return str
+        }
+
+
+    }
+  //  isTeaching(student){
+   //     return student._class.isIn()
+   // }
+}
+
+/*
+ 再写一个Teacher类继承Person类，除了id，name，age属性，也有classes属性。也有一个introduce方法，
+ introduce方法返回一个字符串形如：
+
+ My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2, 3.
+
+ 如果classes的长度为0，就会返回
+
+ My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.
+
+ Teacher还有一个isTeaching方法，传入一个学生，返回true/false。只要学生在classes中的任一个class中，就是在教他。
+ 而学生是否在class中这件事情，应该是Class有一个方法isIn来判断。
+
+
+
+
+*/
+module.exports={
+    Person,
+    Student,
+    Class,
+    Teacher
+}
--- workspace/oop-dojo-master/spec/practice_11/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_11/main_spec.js
@@ -1,7 +1,32 @@
 "use strict";
-
+import {Person,Student,Class,Teacher} from '../../main/practice_11.js'
 describe("OO入门", function(){
     it("对象之间的交互-5", function(){
+        let person=new Person(11,'Tom',21)
+        expect(person.introduce()).toEqual('My name is Tom, I am 21 years old.')
+        let person2=new Person(12,'Tom',21)
+        expect(person.isSamePerson(person2)).toEqual(false)
+
+        let _class=new Class(2)
+        let student=new Student(11,'Tom',21,_class)
+        _class.assignLeader(student)
+        expect(student.introduce()).toEqual('It is not of us.')
+
+        let _class1=new Class(3)
+        let student1=new Student(11,'Tom',21,_class1)
+        _class1.appendMember(student1)
+        expect(student1.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am at Class 3.')
+
+
+        let _class2=new Class(4)
+        let student2=new Student(11,'Tom',21,_class2)
+        _class2.appendMember(student2)
+        _class2.assignLeader(student2)
+        expect(student2.introduce()).toEqual('My name is Tom, I am 21 years old. I am a student, I am the leader of Class 4.')
+
+        let classes=_class
+        let teacher=new Teacher(23,'Tom',21,classes)
+        expect(teacher.introduce()).toEqual('My name is Tom, I am 21 years old. I am a teacher, I teach class 2 3 4')
 
     });
 });
