1c8534cb73e5d6cfe191f0142808e5e107b2e5ff
zero654655902 <929936270@qq.com>
2017-07-24 15:59:30 +0800

    refactor practice_2 +10min]

--- workspace/oop-dojo-java-master/src/main/java/practice_2/Student.java
+++ workspace/oop-dojo-java-master/src/main/java/practice_2/Student.java
@@ -3,18 +3,20 @@ package practice_2;
 /**
  * Created by lwan on 24/07/2017.
  */
-public class Student extends Person {
+public class Student{
     private String name;
     private int age;
     private int klass;
 
     public Student(String name, int age, int klass) {
-        super(name,age);
+        //super(name,age);
+        this.name=name;
+        this.age=age;
         this.klass = klass;
     }
     public String introduce(){
-        String str=super.introduce();
-        str+=" I am a student, I am at Class "+this.klass;
+        //String str=super.introduce();
+        String str="My name is "+this.name+", I am "+this.age+" years old. I am a student, I am at Class "+this.klass;
         return str;
     }
 
