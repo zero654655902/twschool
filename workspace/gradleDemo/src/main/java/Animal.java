94bbc5a13d3d8cf9ec4b8555314034714660cb38
zero654655902 <929936270@qq.com>
2017-07-24 14:24:30 +0800

    init oop-dojo-java-master

---
+++ workspace/gradleDemo/src/main/java/Animal.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lwan on 24/07/2017.
+ */
+public class Animal {
+    private int age;
+    public Animal(int age) {
+        this.age=age;
+    }
+    
+}
---
+++ workspace/gradleDemo/src/test/java/testAnimal.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+/**
+ * Created by lwan on 24/07/2017.
+ */
+public class testAnimal {
+    @Test public void testAnimalAge(){
+        Animal testAnimal1=new Animal(21);
+    }
+}
