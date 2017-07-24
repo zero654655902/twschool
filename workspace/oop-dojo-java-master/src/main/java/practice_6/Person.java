761573f7c5097c6bad4e95515d1e8eba82e316af
zero654655902 <929936270@qq.com>
2017-07-24 16:50:32 +0800

    practice_6 test pass[expect: 15min,actual:13min]

---
+++ workspace/oop-dojo-java-master/src/main/java/practice_6/Person.java
@@ -0,0 +1,24 @@
+package practice_6;
+
+/**
+ * Created by lwan on 24/07/2017.
+ */
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name=name;
+        this.age=age;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is "+this.name+", I am "+this.age+" years old.";
+    }
+}
---
+++ workspace/oop-dojo-java-master/src/main/java/practice_6/Worker.java
@@ -0,0 +1,16 @@
+package practice_6;
+
+/**
+ * Created by lwan on 24/07/2017.
+ */
+public class Worker extends Person {
+    private String name;
+
+    public Worker(String name, int age) {
+        super(name,age);
+    }
+    public String introduce(){
+        return super.introduce()+" I am a worker, I have a job.";
+    }
+
+}
