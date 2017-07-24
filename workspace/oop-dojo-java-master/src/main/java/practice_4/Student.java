a9bc30e5de1eb7d3d977395122be2e6490da534a
zero654655902 <929936270@qq.com>
2017-07-24 16:23:53 +0800

    practice_4 test  pass [expect:15min, actual:17min]

---
+++ workspace/oop-dojo-java-master/src/main/java/practice_4/Student.java
@@ -0,0 +1,23 @@
+package practice_4;
+
+/**
+ * Created by lwan on 24/07/2017.
+ */
+public class Student extends Person{
+    private String name;
+    private int age;
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        String str=super.introduce();
+        str+=" I am a student, I am at Class "+this.klass;
+        return str;
+    }
+
+
+
+}
