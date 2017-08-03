e3232ef028add70f1df13bd0f351acefe466a309
zero654655902 <929936270@qq.com>
2017-08-03 23:26:55 +0800

    add search student html expect 20min,actual 17min

--- workspace/student-grade-web/js/searchStudent.js
+++ workspace/student-grade-web/js/searchStudent.js
@@ -2,9 +2,14 @@
 //  * Created by lwan on 03/08/2017.
 //  */
 
+
+
+
 var data=localStorage.getItem("data");
 var result=JSON.parse(data);
 console.log(result);
+
+
 
 function FormatStudentMessageToTable(student) {
     const str=`<tr><td>${student.id}</td><td>${student.name}</td><td>${student.phone}</td><td>${student.IdCard}</td><td>${student.city}</td></tr>`;
