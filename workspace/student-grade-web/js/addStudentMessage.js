9bd88c38151c6d8baa904968f900bf39c6537cc0
zero654655902 <929936270@qq.com>
2017-08-03 21:32:12 +0800

    add student message validate [expect 20min,actual27min]

---
+++ workspace/student-grade-web/js/addStudentMessage.js
@@ -0,0 +1,33 @@
+/**
+ * Created by lwan on 03/08/2017.
+ */
+
+$(function () {
+    $('#student_message').validate({
+        rules: {
+            stuName: {required: true},
+            email: {required: true},
+            phone_num: {required: true},
+            IdCard: {required: true},
+            city: {required: true}
+        },
+        messages:{
+            stuName:"请输入学生姓名",
+            email:"请输入学生的正确邮箱",
+            phone_num:"请输入学生的电话号码",
+            IdCard:"请输入学生的身份证号码",
+            city:"请输入学生所在城市"
+        }
+    });
+
+    $('#addStu').submit(function () {
+        event.preventDefault();
+
+        var all_data=$('#student_message').serializeArray();
+        console.log(all_data);
+        let data=JSON.stringify(all_data);
+
+    });
+
+})
+
