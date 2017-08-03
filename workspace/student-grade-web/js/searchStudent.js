79b316e501701c02d3ba0d6611e7a4c641bb1238
zero654655902 <929936270@qq.com>
2017-08-04 00:33:28 +0800

    add search student expect 30min actual 15min

--- workspace/student-grade-web/js/searchStudent.js
+++ workspace/student-grade-web/js/searchStudent.js
@@ -4,36 +4,47 @@
 
 $(function () {
 
+    var all_stu=get_all_stu();
+
+    $("#inquire_summit").click(function () {
+      var inquire_message=$("#inquire").val();
+      console.log(inquire_message);
+      console.log(all_stu);
+
+        for(let j=0;j<all_stu.length;++j)
+        {
+            var stu=all_stu[j];
+           if(inquire_message==stu[0].value)
+           {
+               alert("查找到学生");
+           }
+        }
 
 
+    })
+
+
+});
+
+get_all_stu=function () {
     var data = localStorage.getItem("data");
     var result = JSON.parse(data);
 
     console.log(result);
 
-for(let j=0;j<result.length;++j)
-{
-    var stu=result[j];
-    var Id="stuId";
-    for (let i = 0; i < stu.length; ++i) {
+    for(let j=0;j<result.length;++j)
+    {
+        var stu=result[j];
+        var Id="stuId";
+        for (let i = 0; i < stu.length; ++i) {
 
-        var id = '' + "#" + stu[i].name+j;
-        var stuId =  "#"+Id+j;
-        var idNum="10"+j;
-        $(stuId).html(idNum);
-        $(id).html(stu[i].value);
-    }
+            var id = '' + "#" + stu[i].name+j;
+            var stuId =  "#"+Id+j;
+            var idNum="10"+j;
+            $(stuId).html(idNum);
+            $(id).html(stu[i].value);
+        }
+}
+return result;
 }
 
-
-
-});
-
-
-
-
-
-function FormatStudentMessageToTable(student) {
-    const str=`<tr><td>${student.id}</td><td>${student.name}</td><td>${student.phone}</td><td>${student.IdCard}</td><td>${student.city}</td></tr>`;
-    return str;
-}
