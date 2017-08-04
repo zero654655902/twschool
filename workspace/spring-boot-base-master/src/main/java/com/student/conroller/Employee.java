e5681d0c06ac7ae57ab344208b123e166f9df673
zero654655902 <929936270@qq.com>
2017-08-04 16:07:58 +0800

    add one employee [expect 20min actual 30min]

---
+++ workspace/spring-boot-base-master/src/main/java/com/student/conroller/Employee.java
@@ -0,0 +1,37 @@
+package com.student.conroller;
+
+/**
+ * Created by lwan on 04/08/2017.
+ */
+public class Employee {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private String id;
+private String name;
+private int age;
+
+}
+
+
--- workspace/spring-boot-base-master/src/main/java/com/student/conroller/HelloController.java
+++ workspace/spring-boot-base-master/src/main/java/com/student/conroller/HelloController.java
@@ -2,19 +2,29 @@ package com.student.conroller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
+    private Map<String,Employee> employees=new HashMap<>();
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
         return new ResponseEntity<String>("hello", HttpStatus.OK);
     }
 
-
+    @RequestMapping(value = "/employees",method = RequestMethod.POST)
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        employee.setId("001");
+        employees.put("001",employee);
+        return new ResponseEntity<Employee>(employee,HttpStatus.CREATED);
+    }
 
 
 
