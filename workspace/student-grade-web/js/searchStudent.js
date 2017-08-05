10c39ff2299bbb920b8b96a70de634f8b74a0c60
zero654655902 <929936270@qq.com>
2017-08-05 18:19:44 +0800

    search student by id or name

--- workspace/student-grade-web/js/searchStudent.js
+++ workspace/student-grade-web/js/searchStudent.js
@@ -2,30 +2,41 @@
 //  * Created by lwan on 03/08/2017.
 //  */
 
-$(function () {
+ $(function () {
+     var all_stu = get_all_stu();
+     show_stu_massege(all_stu);
+     $("#inquire_summit").click(function () {
+         var inquire_message = $("#inquire").val();
+         console.log(inquire_message);
+         console.log(all_stu);
 
-    var all_stu = get_all_stu();
-    // var all_stu_format=getStudentFormat(all_stu);
+         for (let j = 0; j < all_stu.length; ++j) {
+             var stu = all_stu[j];
+             console.log(stu.stuName);
+             if (inquire_message === stu.stuName ||inquire_message === stu.id ) {
+                 show_one_sudent(stu);
+                 // alert("查找到学生");
+             }
+         }
+ });
+
+
+})
+
+show_one_sudent=function(student){
+    var one_student=getStudentFormat(student);
+    // console.log(all_stu_format);
+    $("table").html(tableHeard+one_student);
+
+}
+
+show_stu_massege=function(all_stu){
+
     var all_stu_format=show_all_stu_format(all_stu);
     console.log(all_stu_format);
     $("table").html(tableHeard+all_stu_format);
-    // $("").html(all_stu_format);
 
-    $("#inquire_summit").click(function () {
-        var inquire_message = $("#inquire").val();
-        console.log(inquire_message);
-        console.log(all_stu);
-
-        for (let j = 0; j < all_stu.size(); ++j) {
-            var stu = all_stu[j];
-            if (inquire_message == stu[0].value) {
-                alert("查找到学生");
-            }
-        }
-    })
-
-
-});
+}
 const tableHeard=`<tr>` +
         `<th>id</th>` +
         `<th>name</th>` +
